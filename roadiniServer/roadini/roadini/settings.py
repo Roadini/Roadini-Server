d0346e2435e3def5334d6384a5b7fa604a8a42be
Tiago Ramalho <t.ramalho22@ua.pt>
2018-11-08 23:05:23 +0000

    docker compose done

--- roadiniServer/roadini/roadini/settings.py
+++ roadiniServer/roadini/roadini/settings.py
@@ -76,16 +76,31 @@ WSGI_APPLICATION = 'roadini.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'roadinidb',
-        'USER': 'admin',
-        'PASSWORD':'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+DOCKER = True 
+
+if DOCKER:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'roadinidb',
+                'USER': 'admin',
+                'PASSWORD':'admin',
+                'HOST': 'roadini_db',
+                'PORT': '5432',
+                }
+            }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'roadinidb',
+                'USER': 'admin',
+                'PASSWORD':'admin',
+                'HOST': 'localhost',
+                'PORT': '5432',
+                }
+            }
+
 
 
 # Password validation
